08fd891870d19cfa4dd417ee95e737749a668605
Mar-Issah <masy370@gmail.com>
2024-01-24 13:24:37 -0600

    FeatL create SQLAlchemy model named User

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Class User for ORM"""
+""" SQLAlchemy model named User"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
